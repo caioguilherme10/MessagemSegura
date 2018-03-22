554c591c33fcd5b73203be2ac403ba795c44ed56
caioguilherme10 <caioguilhermeuna@gmail.com>
2018-03-22 08:19:35 -0300

    bug nome1

--- js/Principal.js
+++ js/Principal.js
@@ -48,7 +48,7 @@ dbRefList.on('child_added', snap2 => {
     if(name == array2[0].name){
 
     }else{
-        contacts.innerHTML += "<ul  onclick=escolhido('" + name+ "','"+ snap2.val().email +"','"+ snap2.key + "')>"+
+        contacts.innerHTML += "<ul  onclick='escolhido('" + name+ "','"+ snap2.val().email +"','"+ snap2.key + "')'>"+
                         "<li class='contact'>"+
                         "<div class='wrap'>"+
                         "<span class='contact-status online'></span>"+
@@ -73,7 +73,7 @@ dbRefList.on('child_changed', snap2 => {
     if(name == array2[0].name){
 
     }else{
-        contacts.innerHTML += "<ul onclick=escolhido('" + name +"','"+ snap2.val().email +"','"+ snap2.key +"')>"+
+        contacts.innerHTML += "<ul onclick='escolhido('" + name +"','"+ snap2.val().email +"','"+ snap2.key +"')'>"+
                         "<li class='contact'>"+
                         "<div class='wrap'>"+
                         "<span class='contact-status online'></span>"+
